8de3dc19b89b4a159379ec01aefa0905b5688992
Sam Daitzman <sam@daitzman.com>
2018-12-08 14:51:58 -0500

    serves current

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const fs = require('fs')
 const SerialPort = require('serialport')
 const Readline = SerialPort.parsers.Readline
 const _ = require('lodash')
+const express = require('express')
+
 const package = require('./package.json')
+const app = express()
+let port
+let reading
 
 SerialPort.list((err, ports) => {
   let chosenPort = ports[0].comName
@@ -14,7 +19,7 @@ SerialPort.list((err, ports) => {
   console.log('Connecting to ' + chosenPort)
 
   const parser = new Readline('\n')
-  const port = new SerialPort(chosenPort.comName, {baudRate: 115200, parser: parser})
+  port = new SerialPort(chosenPort.comName, {baudRate: 115200, parser: parser})
 
   port.pipe(parser)
 
@@ -22,13 +27,21 @@ SerialPort.list((err, ports) => {
 
 
   port.on('open', () => {
-    console.log('open connection')
+    console.log('Open connection')
     parser.on('data', (data) => {
-      console.log(+data)
-      if(+data > 500) {
-        
-        process.exit()
-      }
+      reading = +data
+      // console.log(reading)
     })
   })
 })
+
+app.get('/', (req, res) => {
+  if(port.readable)
+  res.send('Hello world')
+})
+
+app.get('/reading', (req, res) => {
+  res.send(String(reading))
+})
+
+app.listen(3000)
