cbea1a7f292f33adb6104cfc2819bf6a861a7156
Sam Daitzman <sdaitzman@olin.edu>
2018-12-21 12:53:02 -0500

    vastly improved WiFi reliability and auto-reconnect

--- DrillTweetESP32/DrillTweetESP32.ino
+++ DrillTweetESP32/DrillTweetESP32.ino
@@ -15,14 +15,58 @@ float current;
 
 float alpha = 0.33;
 
+void wifiConnect() {
+  int dotDelay = 500;
+  unsigned long timeOut = millis() + 10 * 1000;
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+
+  WiFi.enableSTA(true);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    if (WiFi.getAutoReconnect()) {
+      WiFi.begin();
+    }
+    delay(dotDelay);
+    dotDelay *= 2;
+    Serial.print(".");
+    if (millis() >= timeOut) {
+      Serial.println("\nWiFi connection attempts have failed. Making one final 10-second connection attempt...");
+      if (WiFi.getAutoReconnect()) {
+        WiFi.begin();
+      }
+      delay(10 * 1000);
+      if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("\nFinal WiFi connection attempt timed out. ESP32 will now reboot...\n\n\n");
+        delay(1000);
+        ESP.restart();
+      }
+    }
+  }
+
+  Serial.println("");
+  Serial.print("WiFi connected with IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
 void serverConnect() {
+  Serial.println("Attempting to connect to server...");
+  // First, make sure we are connected to WiFi
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Not connected to WiFi. Initiating reconnect...");
+    wifiConnect();
+  }
   // Connect to the websocket server
   if (client.connect(host, 8080)) {
     Serial.println("Connected to server. Attempting handshake...");
     serverHandshake();
   } else {
-    Serial.println("Connection to server failed. Trying again after a pause...");
-    delay(1000);
+    Serial.println("Connection to server failed. Trying again...");
+    delay(5);
     serverConnect();
   }
 }
@@ -50,29 +94,7 @@ void setup() {
   analogSetSamples(1);
   analogSetWidth(10);
 
-
-  // We start by connecting to a WiFi network
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.print(ssid);
-
-  WiFi.enableSTA(true);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    if (WiFi.getAutoReconnect()) {
-      WiFi.begin();
-    }
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.print("WiFi connected with IP address: ");
-  Serial.println(WiFi.localIP());
+  wifiConnect();
 
   delay(50);
 
