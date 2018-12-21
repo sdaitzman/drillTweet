24ff37733f631038089997d29a7777fa8824f18a
Sam Daitzman <sam@daitzman.com>
2018-12-20 20:32:26 -0500

    working highspeed comms (server)

--- WebSocketsESP32TestNodeServer/app.js
+++ WebSocketsESP32TestNodeServer/app.js
@@ -5,6 +5,11 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 80 });
 
 wss.on('connection', function connection(ws) {
+  console.log('New Connection')
+  setInterval(() => {
+    ws.send('getReading')
+    console.log(ws)
+  }, 10)
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
